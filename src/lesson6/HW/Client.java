1487788d59b672eb096073bcbf6d2e36ccd23fc1
taqedo <taqedo@gmail.com>
2022-11-25 21:15:16 +0600

    lesson 6 homework.

--- src/lesson6/HW/Client.java
+++ src/lesson6/HW/Client.java
@@ -1,5 +1,73 @@
 package lesson6.HW;
 
-public class Client {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class Client {
+    Scanner scanner = new Scanner(System.in);
+    private final String ADRR = "localhost";
+    private final int PORT = 9393;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public Client() {
+        connect();
+    }
+
+    public void connect() {
+        try {
+            socket = new Socket(ADRR, PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                while (true){
+                    try {
+                        String inStr = in.readUTF();
+                        if (inStr.equalsIgnoreCase("/end")){
+                            break;
+                        }
+                        System.out.println(inStr);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }try {
+                    out.writeUTF("/end");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }).start();
+
+            new Thread(() -> {
+                while (true){
+                    try {
+                        String outStr = scanner.nextLine();
+                        out.writeUTF("client: " + outStr);
+                        if (outStr.equalsIgnoreCase("/end") ){
+                            break;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    out.writeUTF("/end");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Client();
+    }
 }
